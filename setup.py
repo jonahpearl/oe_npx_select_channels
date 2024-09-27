a531efe18ffee8f74b9421b801a86cd93c104854
Jonah Pearl <jonahpearl@g.harvard.edu>
2024-09-27 17:54:36 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='oe_npx_selector',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Connect to Open Ephys and select channels on a Neuropixels probe',
+    author='Jonah Pearl',
+    license='MIT',
+)
